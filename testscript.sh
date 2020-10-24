63401cc3613142ae232d5bed3aed6e9aa835b6fe
vigy5sept <59451557+vigy5sept@users.noreply.github.com>
2020-10-24 17:59:16 -0400

    Update testscript.sh

--- testscript.sh
+++ testscript.sh
@@ -3,4 +3,4 @@ echo "This is a test script from github"
 NUMBER=$(($NUMBERONE + $NUMBERTWO))
 echo $NUMBER
 echo "This was added after the build"
-echo "This job build is done from GIT"
+echo "This job build is done from GIT via webhook"
